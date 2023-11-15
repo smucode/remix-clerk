502393a3fdda49b418ce66e7b696ad8fddbce8b6
SMU <gh@smuco.de>
2023-11-15 14:55:11 +0100

    bump versions. more output in view

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,20 +1,35 @@
-import { SignedIn, SignedOut, RedirectToSignIn, UserButton } from "@clerk/remix";
+import { SignedIn, SignedOut, RedirectToSignIn, UserButton, useAuth, useUser } from "@clerk/remix";
 import { getAuth } from "@clerk/remix/ssr.server";
 import { type LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
 export const loader: LoaderFunction = async (args) => {
   const auth = await getAuth(args);
-  console.log(auth);
-  return null;
+  return { auth };
 };
 
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+  const auth = useAuth();
+  const user = useUser();
   return (
     <div>
       <SignedIn>
         <h1>Index route</h1>
         <p>You are signed in!</p>
         <UserButton />
+        <h1>getAuth:</h1>
+        <pre>
+          <code>{JSON.stringify(data.auth, null, 2)}</code>
+        </pre>
+        <h1>useAuth:</h1>
+        <pre>
+          <code>{JSON.stringify(auth, null, 2)}</code>
+        </pre>
+        <h1>useUser:</h1>
+        <pre>
+          <code>{JSON.stringify(user, null, 2)}</code>
+        </pre>
       </SignedIn>
       <SignedOut>
         <RedirectToSignIn />
